ca705ea2c2b905839afa4d1a75077ffcf12315e7
wba6 <bradenaey1@gmail.com>
2025-01-24 14:28:43 -0500

    Add comments

--- main.cpp
+++ main.cpp
@@ -1,23 +1,40 @@
 #include <iostream>
 #include "vendor/performance-analyzer/Profiler.h"
 
+
 int stringSearch(const std::string &str, const std::string &subStr);
 void function();
+int standardFind(const std::string &str, const std::string &subStr);
 int main() {
 
     // Start the profiler
     Profiler::Get().BeginSession("Main", "profiler-results.json");
+    std::string word, item;
+    getline(std::cin,word);
+    std::cin >> item;
 
-    stringSearch("Hello, World!", "World");
-    function();
+    //output used variables
+    std::cout << "Word: " << word << std::endl;
+    std::cout << "Item: " << item << std::endl;
+
+    int location = stringSearch(word, item);
+    //function();
+    int location2 =standardFind(word, item);
 
     // end the profiler
     Profiler::Get().EndSession();
+    std::cout << "Location: " << location << std::endl;
+    std::cout << "Location2: " << location2 << std::endl;
     return 0;
 }
 
 /*
  * string search function that returns the index of the first occurrence of the substring in the string
+ *
+ * @param str : the string to search in
+ * @param subStr : the substring to search for
+ *
+ * @return the index of the first occurrence of the substring in the string
  */
 int stringSearch(const std::string &str, const std::string &subStr) {
     PROFILE_FUNCTION();
@@ -36,11 +53,15 @@ int stringSearch(const std::string &str, const std::string &subStr) {
     return -1;
 }
 
-void function() {
+/*
+ * Standard string search function that returns the index of the first occurrence of the substring in the string
+ *
+ * @param str : the string to search in
+ * @param subStr : the substring to search for
+ *
+ * @return the index of the first occurrence of the substring in the string
+ */
+int standardFind(const std::string &str, const std::string &subStr) {
     PROFILE_FUNCTION();
-    for (int i = 0; i < 2000000000; ++i) {
-        if(i%2 == 0) {
-            i += i%2;
-        }
-    }
+    return str.find(subStr);
 }
