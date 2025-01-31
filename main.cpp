6349eee62fcf940752d0b377c75029cbe06b6049
wba6 <bradenaey1@gmail.com>
2025-01-31 15:29:37 -0500

    Fix include dir

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-#include "vendor/performance-analyzer/Profiler.h"
+#include "performance-analyzer/Profiler.h"
 
 extern "C" int containsASM(const char* s1, const char* s2);
+int standardContains(const std::string &str, const std::string &subStr);
 int stringSearch(const std::string &str, const std::string &subStr);
 int standardFind(const std::string &str, const std::string &subStr);
 int main() {
 
 
     int found = -1;
+    int found2 = -1;
     int location = -1;
     int location2 = -1;
 
@@ -28,7 +30,7 @@ int main() {
 
     location = stringSearch(word, item);
     location2 = standardFind(word, item);
-
+    found2 = standardContains(word, item);
     {
         PROFILE_SCOPE("ASM Search");
         found = containsASM(word.c_str(), item.c_str());
@@ -39,8 +41,24 @@ int main() {
     std::cout << "Location: " << location << std::endl;
     std::cout << "Location2: " << location2 << std::endl;
     std::cout << "Found: " << found << std::endl;
+    std::cout << "Found2: " << found2 << std::endl;
     return 0;
 }
+
+
+
+/*
+ * standard contains function that returns 1 if the substring is found in the string, 0 otherwise
+ *
+ * @param s1 : the string to search in
+ * @param s2 : the substring to search for
+ *
+ * @return 1 if the substring is found in the string, 0 otherwise
+ */
+ int standardContains(const std::string &str, const std::string &subStr) {
+    PROFILE_FUNCTION();
+    return str.find(subStr) != std::string::npos;
+ }
 
 /*
  * string search function that returns the index of the first occurrence of the substring in the string
