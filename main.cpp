8043c35fcb338cb848308dad81477e97fa259874
wba6 <bradenaey1@gmail.com>
2025-02-05 16:56:52 -0500

    Update cmake

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
+#include "cl.hpp"
+#include "standardFunctions.hpp"
+#include "implimentedFunctions.hpp"
 #include "performance-analyzer/Profiler.h"
 
 extern "C" int containsASM(const char* s1, const char* s2);
-int standardContains(const std::string &str, const std::string &subStr);
-int stringSearch(const std::string &str, const std::string &subStr);
-int standardFind(const std::string &str, const std::string &subStr);
 int main() {
 
 
@@ -30,7 +30,6 @@ int main() {
 
     location = stringSearch(word, item);
     location2 = standardFind(word, item);
-    found2 = standardContains(word, item);
     {
         PROFILE_SCOPE("ASM Search");
         found = containsASM(word.c_str(), item.c_str());
@@ -43,57 +42,4 @@ int main() {
     std::cout << "Found: " << found << std::endl;
     std::cout << "Found2: " << found2 << std::endl;
     return 0;
-}
-
-
-
-/*
- * standard contains function that returns 1 if the substring is found in the string, 0 otherwise
- *
- * @param s1 : the string to search in
- * @param s2 : the substring to search for
- *
- * @return 1 if the substring is found in the string, 0 otherwise
- */
- int standardContains(const std::string &str, const std::string &subStr) {
-    PROFILE_FUNCTION();
-    return str.find(subStr) != std::string::npos;
- }
-
-/*
- * string search function that returns the index of the first occurrence of the substring in the string
- *
- * @param str : the string to search in
- * @param subStr : the substring to search for
- *
- * @return the index of the first occurrence of the substring in the string
- */
-int stringSearch(const std::string &str, const std::string &subStr) {
-    PROFILE_FUNCTION();
-
-    int strLen = str.length();
-    int subStrLen = subStr.length();
-    for (int i = 0; i < strLen - subStrLen; i++) {
-        int j = 0;
-        while (j < subStrLen && str[i + j] == subStr[j]) {
-            j++;
-        }
-        if (j == subStrLen) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-/*
- * Standard string search function that returns the index of the first occurrence of the substring in the string
- *
- * @param str : the string to search in
- * @param subStr : the substring to search for
- *
- * @return the index of the first occurrence of the substring in the string
- */
-int standardFind(const std::string &str, const std::string &subStr) {
-    PROFILE_FUNCTION();
-    return str.find(subStr);
 }
