f6fb7337152cfeaa6b501d67ec83465b78525ffc
wba6 <bradenaey1@gmail.com>
2025-02-07 14:27:32 -0500

    Add file read ability

--- main.cpp
+++ main.cpp
@@ -1,38 +1,26 @@
 #include <iostream>
+#include <fstream>
 #include "cl.hpp"
 #include "standardFunctions.hpp"
 #include "implimentedFunctions.hpp"
 #include "performance-analyzer/Profiler.h"
 
 extern "C" int containsASM(const char* s1, const char* s2);
+std::string loadFile(std::string filename);
 int main() {
 
-
-    int found = -1;
-    int found2 = -1;
-    int location = -1;
-    int location2 = -1;
-
-    std::string word, item;
-    getline(std::cin,word);
-    std::cin >> item;
-
-    //output used variables
-    std::cout << "Word: " << word << std::endl;
-    std::cout << "Item: " << item << std::endl;
-
-    //output used variables
-    std::cout << "Word: " << word << std::endl;
-    std::cout << "Item: " << item << std::endl;
+    std::string str = loadFile("example.txt");
+    std::string toFind = "hello";
+    int location{0}, location2{0}, found{0}, found2{0};
 
     // Start the profiler
     Profiler::Get().BeginSession("Main", "profiler-results.json");
 
-    location = stringSearch(word, item);
-    location2 = standardFind(word, item);
+    location = stringSearch(str, toFind);
+    location2 = standardFind(str, toFind);
     {
         PROFILE_SCOPE("ASM Search");
-        found = containsASM(word.c_str(), item.c_str());
+        found = containsASM(str.c_str(), toFind.c_str());
     }
 
     // end the profiler
@@ -42,4 +30,14 @@ int main() {
     std::cout << "Found: " << found << std::endl;
     std::cout << "Found2: " << found2 << std::endl;
     return 0;
+}
+
+std::string loadFile(std::string filename){
+    std::ifstream t(filename);
+    t.seekg(0, std::ios::end);
+    size_t size = t.tellg();
+    std::string buffer(size, ' ');
+    t.seekg(0);
+    t.read(&buffer[0], size);
+    return std::move(buffer);
 }
