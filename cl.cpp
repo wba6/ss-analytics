fc182336d14332cf7679893cd13026a4a43cb2ed
wba6 <bradenaey1@gmail.com>
2025-02-10 18:36:43 -0500

    add more timers

--- cl.cpp
+++ cl.cpp
@@ -1,3 +1,6 @@
+#include "vendor/performance-analyzer/Profiler.h"
+#include "vendor/performance-analyzer/Timer.h"
+#include <ctime>
 #include <iostream>
 #define CL_HPP_TARGET_OPENCL_VERSION 300
 #define CL_HPP_ENABLE_EXCEPTIONS
@@ -32,12 +35,17 @@ int clSearch(const std::string& str,const std::string& substr){
             }
         }
     )";
+    Timer* timer = new Timer("Buffers");
     cl::Program program(context,kernalSource,true);
+    program.build();
+    delete timer;
 
+    timer = new Timer("Buffers");
     //data buffers
     cl::Buffer d_str(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, str.size() + 1, (void*)str.c_str());
     cl::Buffer d_substr(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, substr.size() + 1, (void*)substr.c_str());
     cl::Buffer d_result(context, CL_MEM_WRITE_ONLY, sizeof(int));
+    delete timer;
 
     //create kernel
     cl::compatibility::make_kernel<cl::Buffer,cl::Buffer,cl::Buffer> kernel(program, "search");
@@ -48,10 +56,9 @@ int clSearch(const std::string& str,const std::string& substr){
 
         //run kernel
         kernel(cl::EnqueueArgs(queue,cl::NDRange(str.size())),d_str,d_substr,d_result);
-
+    }
         //read result
         int result = -1;
         cl::copy(queue, d_result,&result,&result + 1);
         return result;
-    }
 }
