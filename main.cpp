042072e94bed651999bbe93429b90c7318bc70cd
William Aey <william@mac.lan>
2025-03-25 11:59:44 -0700

    Add vulkan files

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <iostream>
 #include <fstream>
+#include "vkSearch.hpp"
 #include "cl.hpp"
 #include "standardFunctions.hpp"
 #include "implimentedFunctions.hpp"
@@ -10,15 +11,16 @@
 std::string loadFile(std::string filename);
 int main() {
 
-    std::string str = loadFile("500MB_random.txt");
+    std::string str = loadFile("../500MB_random.txt");
     std::string toFind = "belkjdalfjlkj;sdf";
     int location{0}, location2{0},location3{0}, found{0}, found2{0};
     // Start the profiler
-    Profiler::Get().BeginSession("Main", "profiler-results.json");
+    Profiler::Get().BeginSession("Main", "../profiler-results.json");
 
     location = stringSearch(str, toFind);
     location2 = standardFind(str, toFind);
     location3 = clSearch(str, toFind);
+    //found = vulkanStringSearch(str, toFind);
     found2 = standardContains(str, toFind);
 
     // end the profiler
