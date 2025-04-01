d2db5538e12fd79aaa9aadeae57324ac07f8f427
William <bradenaey1@gmail.com>
2025-03-31 20:05:56 -0400

    Refactor variable names

--- main.cpp
+++ main.cpp
@@ -13,24 +13,24 @@ int main() {
 
     std::string str = loadFile("500MB_random.txt");
     std::string toFind = "wj%|0fY41AN";
-    int location{0}, location2{0},location3{0}, found{0}, found2{0}, found3{0};
+    int stringSearchManual{0}, standardLibFind{0}, openCLSearch{0}, vkSearchResult{0}, standardContainsResult{0}, standardFindAllResult{0};
     // Start the profiler
     Profiler::Get().BeginSession("Main", "../profiler-results.json");
 
-    location = stringSearch(str, toFind);
-    location2 = standardFind(str, toFind);
-    location3 = clSearch(str, toFind);
-    //found = vulkanStringSearch(str, toFind);
-    found2 = standardContains(str, toFind);
-    found3 = standardFindAll(str,toFind);
+    stringSearchManual = stringSearch(str, toFind);
+    standardLibFind = standardFind(str, toFind);
+    openCLSearch = clSearch(str, toFind);
+    //vkSearchResult = vulkanStringSearch(str, toFind);
+    standardContainsResult = standardContains(str, toFind);
+    standardFindAllResult = standardFindAll(str,toFind);
 
     // end the profiler
     Profiler::Get().EndSession();
-    std::cout << "Location: " << location << std::endl;
-    std::cout << "Location2: " << location2 << std::endl;
-    std::cout << "Location3: " << location3 << std::endl;
-    std::cout << "Found2: " << found2 << std::endl;
-    std::cout << "Found3: " << found3 << std::endl;
+    std::cout << "Location of string search manual: " << stringSearchManual << std::endl;
+    std::cout << "Location of standard find: " << standardLibFind << std::endl;
+    std::cout << "Location OpenCL Search: " << openCLSearch << std::endl;
+    std::cout << "Standard contains: " << standardContainsResult << std::endl;
+    std::cout << "Location of standard find all: " << standardFindAllResult << std::endl;
     return 0;
 }
 
